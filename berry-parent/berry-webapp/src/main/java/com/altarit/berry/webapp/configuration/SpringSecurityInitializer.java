87a4bdbccdd5470c6752e7e5f1d567c7b5ba0802
altarit <mail@altarit.com>
2016-10-16 15:43:37 +0300

    Basic auth

---
+++ berry-parent/berry-webapp/src/main/java/com/altarit/berry/webapp/configuration/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.altarit.berry.webapp.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
